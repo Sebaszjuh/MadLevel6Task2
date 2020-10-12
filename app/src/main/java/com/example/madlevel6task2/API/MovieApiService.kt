add0e403dcab336cc55b39125e3eb98bbe012bf3
griends1 <Steelpan1>
2020-10-12 22:43:15 +0200

    Added API service and api class

---
+++ app/src/main/java/com/example/madlevel6task2/API/MovieApiService.kt
@@ -0,0 +1,15 @@
+package com.example.madlevel6task2.API
+
+import com.example.madlevel6task2.BuildConfig
+import com.example.madlevel6task2.model.Movies
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val API_KEY: String = BuildConfig.API_KEY
+interface MovieApiService {
+
+    @GET("discover/movie?api_key=$API_KEY")
+    fun getMoviesByYear(@Query("primary_release_year") year: String): Call<Movies>
+}
+
