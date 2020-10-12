f95349bf509525f1e5749520e4c6dca24ce32d6e
griends1 <Steelpan1>
2020-10-12 22:03:46 +0200

    Data models

---
+++ app/src/main/java/com/example/madlevel6task2/model/Movie.kt
@@ -0,0 +1,15 @@
+package com.example.madlevel6task2.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Movie(
+    @SerializedName("title") var title: String,
+    @SerializedName("release_date") var releaseDate: String,
+    @SerializedName("overview") var overview: String,
+    @SerializedName("vote_average") var rating: Double,
+    @SerializedName("poster_path") var poster: String,
+    @SerializedName("backdrop_path") var backdrop: String
+) : Parcelable
